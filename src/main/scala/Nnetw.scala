ae3c42a5d9f321957106b8b3d1c65eb7baa768f4
Steffen Ripke <Steffen@Ripke-Home.de>
2015-12-13 11:18:37 +0100

    matrice basics added

--- src/main/scala/Nnetw.scala
+++ src/main/scala/Nnetw.scala
@@ -2,15 +2,27 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.mllib.linalg.{Matrix, Matrices}
+import org.apache.spark.mllib.linalg.distributed.RowMatrix
 
 object Nnetw {
   def main(args: Array[String]) {
-    val logFile = "/usr/local/Cellar/apache-spark/1.5.2/README.md" // Should be some file on your system
-    val conf = new SparkConf().setAppName("Neural Network Application")
+    val conf = new SparkConf().setAppName("Neural Network")
     val sc = new SparkContext(conf)
-    val logData = sc.textFile(logFile, 2).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+
+    val rows = sc.textFile("m1.txt").map { line =>
+      val values = line.split(' ').map(_.toDouble)
+      Vectors.sparse(values.length,
+                    values.zipWithIndex.map(e => (e._2, e._1)).filter(_._2 != 0.0))
+    }
+
+    // Create dense vectors for input a and biases b.
+    val a: Vector = Vectors.dense(0.1, 0.0, 0.3)
+    val b: Vector = Vectors.dense(0.3, 0.2, 0.1)
+
+    // Create a dense matrix w for the weights)
+    val w: Matrix = Matrices.dense(3, 2, Array(0.1, 0.3, 0.5, 0.2, 0.4, 0.6))
+    //println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
   }
 }
